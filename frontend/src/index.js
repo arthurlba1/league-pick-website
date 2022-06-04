41d2f981d4eac50d8cb708b152529956a5784e8e
arthurlba1 <1arthurluiz1@gmail.com>
2022-06-03 22:03:02 -0300

    fixed import warning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ConfigProvider } from 'antd';
 import ptBR from 'antd/lib/locale/pt_BR';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
