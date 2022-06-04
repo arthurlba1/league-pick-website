e7c13381e8b89e3761822394577fb5a31bba3870
arthurlba1 <1arthurluiz1@gmail.com>
2022-06-03 22:03:13 -0300

    created basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
-function App() {
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+function App () {
   return (
-    <div className="App">
-      <h1>
-        Learn React
-      </h1>
-    </div>
+    <Layout>
+      <Header
+        style={{
+          position: 'fixed',
+          zIndex: 1,
+          width: '100%',
+        }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content style={{ marginTop: 64 }} >
+        <div>
+          Content
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
