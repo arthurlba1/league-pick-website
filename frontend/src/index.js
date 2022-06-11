7fff0aae82b82a3ae27cfcb29ec0a0d53dd9e83a
arthurlba1 <1arthurluiz1@gmail.com>
2022-06-10 22:02:34 -0300

    initial state of root files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { ConfigProvider } from 'antd';
-import ptBR from 'antd/lib/locale/pt_BR';
 
-import 'antd/dist/antd.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ConfigProvider locale={ptBR}>
       <App />
-    </ConfigProvider>
   </React.StrictMode>
 );
